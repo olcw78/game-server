4de6eef850286c0a313366126ba27a5b3bde2014
highp0912@pm.me <yoonsang Lee>
2024-04-09 23:01:02 +0900

    init

---
+++ dummy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+string host = Dns.GetHostName();
+IPHostEntry ipHostEntry = Dns.GetHostEntry(host);
+
+// foreach (var ad in ipHostEntry.AddressList)
+//     System.Console.WriteLine(ad);
+IPAddress mezelf = ipHostEntry.AddressList[0];
+IPEndPoint ipEndPoint = new(mezelf, 3080);
+
+try
+{
+    Socket socket = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    await socket.ConnectAsync(ipEndPoint);
+
+    System.Console.WriteLine($"connected {socket.RemoteEndPoint}");
+
+    byte[] buf = new byte[1024];
+
+    byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes("Message from dummy");
+    await socket.SendAsync(sendBytes);
+
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+catch (Exception ex)
+{
+    System.Console.WriteLine(ex.Message);
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+string host = Dns.GetHostName();
+IPHostEntry ipHostEntry = Dns.GetHostEntry(host);
+IPAddress mezelf = ipHostEntry.AddressList[0];
+IPEndPoint iPEndPoint = new(mezelf, 3080);
+
+Socket socket = new(iPEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+try
+{
+    socket.Bind(iPEndPoint);
+    socket.Listen(backlog: 10);
+
+    do
+    {
+        Console.WriteLine($"Listening... at 3080");
+        var accepted = await socket.AcceptAsync();
+
+        byte[] buf = new byte[1024];
+        int recvBytes = await accepted.ReceiveAsync(buf);
+        if (recvBytes > 0)
+        {
+            string msg = System.Text.Encoding.UTF8.GetString(buf, 0, recvBytes);
+            Console.WriteLine($"Received: {msg}");
+
+            byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(
+                "Jij hebt wel op de ober geconnecteerd."
+            );
+            await accepted.SendAsync(sendBytes);
+        }
+
+        accepted.Shutdown(SocketShutdown.Both);
+        accepted.Close(100);
+    } while (true);
+}
+catch (Exception ex)
+{
+    System.Console.WriteLine(ex.Message);
+}
