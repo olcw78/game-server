72f9ee211db4ba9ad6f3d467e19f8ecb4a0512fd
highp0912@pm.me <yoonsang Lee>
2024-04-12 22:14:20 +0900

    restore: dummy

--- dummy/Program.cs
+++ dummy/Program.cs
@@ -1,4 +1,6 @@
-﻿using gs.dummy;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using gs.shared;
 
 string envPath = Path.Join(Directory.GetCurrentDirectory(), ".env");
@@ -6,5 +8,36 @@ Console.WriteLine(envPath);
 Dotenv.Load(envPath);
 
 int port = Dotenv.GetI("PORT") ?? 3000;
-Session session = new(port);
-await session.ConnectAsync();
+
+Socket server;
+string host = Dns.GetHostName();
+IPHostEntry ipHostEntry = Dns.GetHostEntry(host);
+IPAddress localhost = ipHostEntry.AddressList[0];
+IPEndPoint ipEndPoint = new IPEndPoint(localhost, port);
+
+while (true) {
+  try {
+    server = new Socket(ipEndPoint.AddressFamily, SocketType.Stream,
+      ProtocolType.Tcp);
+    await server.ConnectAsync(ipEndPoint);
+    Console.WriteLine($"connected {server.RemoteEndPoint}");
+
+    byte[] buf = Encoding.UTF8.GetBytes("dummy client is connected");
+    server.Send(buf);
+
+    buf = new byte[1024];
+    int recvBytes = server.Receive(buf);
+    if (recvBytes > 0) {
+      string recvContent = Encoding.UTF8.GetString(buf, 0, recvBytes);
+      Console.WriteLine($"[{recvBytes}]received: {recvContent}");
+    }
+
+    server.Shutdown(SocketShutdown.Both);
+    server.Close();
+
+    Thread.Sleep(1000);
+  }
+  catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+  }
+}
